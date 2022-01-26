76bce95bb4990dffdb267968e4c10f210635cfc5
Jaks024 <42505518+Jaks024@users.noreply.github.com>
2022-01-25 22:47:39 -0500

    added add page styling

--- src/AddPage.js
+++ src/AddPage.js
@@ -1,36 +1,42 @@
 import React from 'react';
+import './AddPage.css'
 
 class AddPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <span>Date</span>
-                    <input></input>
+            <div className="AddPage">
+                <div className="AddPage-header">
+                    Add Entry
                 </div>
-                <div>
-                    <span>Category</span>
-                    <input></input>
+                <div className="AddPage-form">
+                    <div>
+                        <span>Date</span>
+                        <input></input>
+                    </div>
+                    <div>
+                        <span>Category</span>
+                        <input></input>
+                    </div>
+                    <div>
+                        <span>Location</span>
+                        <input></input>
+                    </div>
+                    <div>
+                        <span>Income</span>
+                        <input></input>
+                    </div>
+                    <div>
+                        <span>Expense</span>
+                        <input></input>
+                    </div>
+                    <div>
+                        <span>Description</span>
+                        <input></input>
+                    </div>
+                    <button>Add</button>
+                    <button>Clear</button>
                 </div>
-                <div>
-                    <span>Location</span>
-                    <input></input>
-                </div>
-                <div>
-                    <span>Income</span>
-                    <input></input>
-                </div>
-                <div>
-                    <span>Expense</span>
-                    <input></input>
-                </div>
-                <div>
-                    <span>Description</span>
-                    <input></input>
-                </div>
-                <button>Add</button>
-                <button>Clear</button>
             </div>
         );
     }
