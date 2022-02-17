0ab64a0d83b6ac865ec087c5738ff8bb1e8cce08
Jaks024 <42505518+Jaks024@users.noreply.github.com>
2022-02-16 22:50:03 -0500

    added build cmd

---
+++ client/build/precache-manifest.5ea0aa5bc838498d69b9b6a172462df9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ba92d76695441e948e4d0a80afeb7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c60fca3ec24cfb72956",
+    "url": "/static/css/2.534f228d.chunk.css"
+  },
+  {
+    "revision": "a967cf0bc01f5ad8ed07",
+    "url": "/static/css/main.bcd47ff7.chunk.css"
+  },
+  {
+    "revision": "5c60fca3ec24cfb72956",
+    "url": "/static/js/2.c6e9572b.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.c6e9572b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebf9060c0e40b675409",
+    "url": "/static/js/3.cfbf47f5.chunk.js"
+  },
+  {
+    "revision": "a967cf0bc01f5ad8ed07",
+    "url": "/static/js/main.0992edf5.chunk.js"
+  },
+  {
+    "revision": "9a305d1b7ff999e54434",
+    "url": "/static/js/runtime-main.922531a7.js"
+  }
+]);
